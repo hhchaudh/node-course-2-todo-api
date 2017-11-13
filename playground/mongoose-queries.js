2184cdce9a81e966952001c66b1420cd9862bd2a
hhchaudh <haaris.h.chaudhry@gmail.com>
2017-11-13 01:02:19 -0600

    Add queries file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,42 @@
+/*jshint esversion: 6 */
+/* jshint node: true */
+
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '5a0935d98903acdc782c23c511';
+//
+// if (!ObjectID.isValid(id)) {
+//     console.log('ID not valid');
+// }
+
+// Todo.find({
+//     _id: id //automatically converted to ObjectID
+//
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//     _id: id //automatically converted to ObjectID
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if(!todo) {
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo by Id', todo);
+// }).catch((e) => console.log(e));
+let id = '5a091164a9cf0c486623e2ca';
+
+User.findById(id).then((user) => {
+    if(!user) {
+        return console.log('Id not found');
+    }
+    console.log('User by Id', user);
+}).catch((e) => console.log(e));
